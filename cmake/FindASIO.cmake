e8811c72384d99c65cd5f126fbbb7f0f51fbed5b
s.kolesnik <7fosGbc9S>
2021-04-12 12:56:16 +0500

    modified FindASIO.cmake

--- cmake/FindASIO.cmake
+++ cmake/FindASIO.cmake
@@ -29,23 +29,20 @@ find_package_handle_standard_args(ASIO
         FAIL_MESSAGE "ASIO was not found")
 
 if (${ASIO_FOUND})
-    add_library(ASIO::ASIO INTERFACE IMPORTED)
-    set_target_properties(ASIO::ASIO PROPERTIES
-            INTERFACE_COMPILE_DEFINITIONS "ASIO_STANDALONE"
-            INTERFACE_INCLUDE_DIRECTORIES ${ASIO_INCLUDE_DIR})
+    if (NOT TARGET ASIO::ASIO)
+        add_library(ASIO::ASIO INTERFACE IMPORTED)
+        set_target_properties(ASIO::ASIO PROPERTIES
+                INTERFACE_COMPILE_DEFINITIONS "ASIO_STANDALONE"
+                INTERFACE_INCLUDE_DIRECTORIES ${ASIO_INCLUDE_DIR})
 
-    if(WIN32)
-        target_link_libraries(ASIO::ASIO
-                INTERFACE
-                Threads::Threads
-                ws2_32.lib
-                mswsock.lib
-                )
-    endif()
-
+        if(WIN32)
+            target_link_libraries(ASIO::ASIO INTERFACE
+                    Threads::Threads
+                    ws2_32.lib
+                    mswsock.lib
+                    )
+            target_compile_definitions(ASIO::ASIO INTERFACE _WIN32_WINDOWS _WIN32_WINNT=0x0601)
+        endif()
         message(STATUS "Asio include dir: ${ASIO_INCLUDE_DIR}")
-endif ()
-
-if (WIN32)
-    target_compile_definitions(ASIO::ASIO INTERFACE _WIN32_WINDOWS _WIN32_WINNT=0x0601)
+    endif()
 endif ()
